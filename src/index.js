b0041c2fc66d8cfead07930a0457ba8cd597cacb
carvilsi <carvilsi.mail.list@gmail.com>
2024-03-08 13:15:34 +0100

    almost happy

--- src/index.js
+++ src/index.js
@@ -1,112 +1,109 @@
-arrayOfFaces = [];
-textFaces = '';
-faceWithName = undefined;
+import faces from './faces.js';
 
-export default class TxTFaces {
-    #arrayOfFaces = [];
-    #textFaces = '';
-    #faceWithName = undefined;
+let arrayOfFaces = [];
+let textFaces = '';
+let faceWithName = undefined;
 
-    constructor() {
-        const txtFaces = new TxTFaces();
-        return txtfaces;
+
+// Gives a "random" number between 0 and length (inclusive)
+function random(length) {
+    return Math.floor(Math.random() * length);
+}
+
+function formatIt(element, level) {
+    textFaces = `${textFaces}${'\t'.repeat(level)}${element}\n`;
+}
+
+function prettyColletAllFaces(obj, stack) {
+    const level = stack.split(',').length - 1;
+
+    if (Array.isArray(obj)) {
+        formatIt(obj.join(' '), level);
     }
 
-    // Gives a "random" number between 0 and length (inclusive)
-    #random(length) {
-        return Math.floor(Math.random() * length);
-    }
-    
-    #formatIt(element, level) {
-        textFaces = `${textFaces}${'\t'.repeat(level)}${element}\n`;
-    }
-    
-    #prettyColletAllFaces(obj, stack) {
-        const level = stack.split(',').length - 1;
-    
-        if (Array.isArray(obj)) {
-            formatIt(obj.join(' '), level);
-        }
-    
-        for (const property in obj) {
-            if (Object.prototype.hasOwnProperty.call(obj, property)) {
-                if (!Array.isArray(obj)) {
-                    formatIt(`${property.replaceAll('_', ' ')}:`, level);
-                }
-    
-                if (typeof obj[property] === 'object') {
-                    prettyColletAllFaces(obj[property], `${stack},${property}`);
-                }
+    for (const property in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, property)) {
+            if (!Array.isArray(obj)) {
+                formatIt(`${property.replaceAll('_', ' ')}:`, level);
+            }
+
+            if (typeof obj[property] === 'object') {
+                prettyColletAllFaces(obj[property], `${stack},${property}`);
             }
         }
     }
-    
-    export function prettyPrintFaces() {
-        textFaces = '';
-        prettyColletAllFaces(obj, '');
-        return textFaces;
+}
+
+function collectFacesToArray(obj) {
+    if (Array.isArray(obj)) {
+        arrayOfFaces.push(obj);
     }
-    
-    function collectFacesToArray(obj) {
-        if (Array.isArray(obj)) {
-            arrayOfFaces.push(obj);
-        }
-    
-        for (const property in obj) {
-            if (Object.prototype.hasOwnProperty.call(obj, property)) {
-                if (typeof obj[property] === 'object') {
-                    collectFacesToArray(obj[property]);
-                }
+
+    for (const property in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, property)) {
+            if (typeof obj[property] === 'object') {
+                collectFacesToArray(obj[property]);
             }
         }
     }
-    
-    export function getArrayOfFaces(facesSource) {
-        arrayOfFaces = [];
-        collectFacesToArray(facesSource, arrayOfFaces);
-        return arrayOfFaces.flat(1);
-    }
-    
-    export function getRandomFace(obj) {
-        if (!Array.isArray(obj)) {
-            // Count number of properties of obj and get a random
-            // send it again here
-            return getRandomFace(obj[Object.keys(obj)[random(Object.keys(obj).length)]]);
-        } else if (typeof obj === 'object') {
-            if (obj.length === 1) {
-                return obj[0];
-            }
-            // a random face from faces array
-            return obj[random(obj.length)];
+}
+
+function getRandomFace(obj) {
+    if (!Array.isArray(obj)) {
+        // Count number of properties of obj and get a random
+        // send it again here
+        return getRandomFace(obj[Object.keys(obj)[random(Object.keys(obj).length)]]);
+    } else if (typeof obj === 'object') {
+        if (obj.length === 1) {
+            return obj[0];
         }
+        // a random face from faces array
+        return obj[random(obj.length)];
     }
-    
-    function collectFaceByName(obj, name) {
-        for (const property in obj) {
-            if (Object.prototype.hasOwnProperty.call(obj, property)) {
-                if (!Array.isArray(obj)) {
-                    const regexp = new RegExp(name, 'g');
-                    if (property.match(regexp)?.length) {
-                        if (Array.isArray(obj[property])) {
-                            faceWithName = obj[property].length === 1 ?
-                                obj[property][0] :
-                                obj[property][random(obj[property].length)];
-                        } else {
-                            collectFaceByName(obj[property], name);
-                        }
+}
+
+function collectFaceByName(obj, name) {
+    for (const property in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, property)) {
+            if (!Array.isArray(obj)) {
+                const regexp = new RegExp(name, 'g');
+                if (property.match(regexp)?.length) {
+                    if (Array.isArray(obj[property])) {
+                        faceWithName = obj[property].length === 1 ?
+                            obj[property][0] :
+                            obj[property][random(obj[property].length)];
+                    } else {
+                        collectFaceByName(obj[property], name);
                     }
                 }
-    
-                if (typeof obj[property] === 'object') {
-                    collectFaceByName(obj[property], name);
-                }
+            }
+
+            if (typeof obj[property] === 'object') {
+                collectFaceByName(obj[property], name);
             }
         }
     }
-    
-    export function getFaceByName(obj, name) {
+}
+
+const txtface = {
+    get all() {
+        textFaces = '';
+        prettyColletAllFaces(faces, '');
+        return textFaces;
+    },
+    get array() {
+        arrayOfFaces = [];
+        collectFacesToArray(faces);
+        return arrayOfFaces.flat(1);
+    },
+    get rand() {
+        return getRandomFace(faces);
+    },
+    like(name) {
         faceWithName = undefined;
-        collectFaceByName(obj, name);
+        collectFaceByName(faces, name);
         return faceWithName;
     }
-    
+};
+
+export default txtface;
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,33 +1,33 @@
 /* eslint no-console: "off" */
 
 import test from 'ava';
-import txtfaces from '../src/index.js';
+import txtface from '../src/index.js';
 
 test('should retrieve an array of faces and a random face', (t) => {
-    const arrayOfFaces = txtfaces.array(); 
+    const arrayOfFaces = txtface.array; 
 
     t.true(arrayOfFaces.length > 0, 'the array of faces is empty');
 
-    const face = txtfaces.rand();
+    const face = txtface.rand;
 
     t.true(arrayOfFaces.indexOf(face) >= 0, 'face not found at faces array');
 });
 
 test('should retrieve string with all faces fo pretty print', (t) => {
-    const prettyString = txtfaces.prettyPrint();
+    const prettyString = txtface.all;
     console.log(prettyString);
 
     t.assert(typeof prettyString === 'string');
 });
 
 test('should retrieve a random face by name', (t) => {
-    const face = txtfaces.get('embarrassed');
+    const face = txtface.like('embarrassed');
 
     t.true([ ':$', '://)', '://3' ].indexOf(face) >= 0);
 });
 
 test('should not retrieve a random face by name because does not exists', (t) => {
-    const face = txtfaces.get('foz');
+    const face = txtface.like('foz');
 
     t.is(typeof face, 'undefined');
 });
