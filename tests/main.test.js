a840df30c2512d9b43bb5d367a94e3447214b423
carvilsi <carvilsi.mail.list@gmail.com>
2024-03-13 17:13:32 +0100

    more faces.. too many faces

--- tests/main.test.js
+++ tests/main.test.js
@@ -3,7 +3,7 @@
 import test from 'ava';
 import facetxt from '../src/index.js';
 
-const TOTAL_FACES = 352;
+const TOTAL_FACES = 408;
 const WHOLE_EMBARRASSED_FACES = [
       ':$',          '://)',
       '://3',        '(^^ゞ',
