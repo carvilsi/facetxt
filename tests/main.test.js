3aeb5eaa9f5da55e8263143e9456c8c069598ace
carvilsi <carvilsi.mail.list@gmail.com>
2024-03-17 10:43:27 +0100

    adding more faces :)

--- tests/main.test.js
+++ tests/main.test.js
@@ -3,7 +3,7 @@
 import test from 'ava';
 import facetxt from '../src/index.js';
 
-const TOTAL_FACES = 475;
+const TOTAL_FACES = 489;
 const WHOLE_EMBARRASSED_FACES = [
     ':$', '://)',
     '://3', '(^^ゞ',
