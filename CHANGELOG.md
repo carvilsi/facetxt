a86495c15380d9407d931af76c02f13f7e959e98
carvilsi <carvilsi.mail.list@gmail.com>
2024-03-21 12:29:21 +0100

    ready for merge to develop and release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,15 @@
 # Changelog
 
-# [v1.0.0](https://github.com/carvilsi/pwyll-cli/releases/tag/v4.1.1) (2024-03-20)
+# [v1.1.0](https://github.com/carvilsi/facetxt/releases/tag/v1.1.0) (2024-03-21)
+
+- adding 2 faces 
+- added usage message for cli
+- more test coverage for cli
+- adding pre-release script
+- Created CODE_OF_CONDUCT.md
+- adding CONTRIBUTING.md
+
+# [v1.0.0](https://github.com/carvilsi/facetxt/releases/tag/v1.0.0) (2024-03-20)
 
 - first release with 537 faces 
 
--- pre-release.sh
+++ pre-release.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 CHNGLG=CHANGELOG.md
-RDM=README.md
+README_FILE=README.md
+TEST_FILE=tests/main.test.js
 NAME=$(node -e "console.log(require('./package.json').name)")
 VERSION=$(node -e "console.log(require('./package.json').version)")
 today=$(date +%Y-%m-%d)
 line="# [v${VERSION}](https:\/\/github.com\/carvilsi\/${NAME}\/releases\/tag\/v${VERSION}) (${today})"
 last_release_date=$(head -n 3 CHANGELOG.md | tail -1 | awk '{ print $3 }' | sed 's/(\|)//g')
 commit_message=$(git log --after=${last_release_date} --format='- %s' | grep -v 'Merge' |  sed '{:q;N;s/\n//g;t q}')
-total_faces=$(awk '/TOTAL_FACES =/ { print $4 }' tests/main.test.js | sed 's/;//g');
-sed -i 's/with [0-9]*/with ${total_faces}/g' README.md
 
-#sed -i '2s/^/\nnewchangelogentry\n/' $CHNGLG
-#sed -i "s/newchangelogentry/${line}\n\n- ${commit_message}/g" $CHNGLG
+total_faces=$(awk '/TOTAL_FACES =/ { print $4 }' $TEST_FILE | sed 's/;//g');
+sed -i "s/Now with [0-9]* awesome faces/Now with ${total_faces} awesome faces/g" $README_FILE
+
+sed -i '2s/^/\nnewchangelogentry\n/' $CHNGLG
+sed -i "s/newchangelogentry/${line}\n\n- ${commit_message}/g" $CHNGLG
 
