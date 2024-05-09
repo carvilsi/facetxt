2689cde8f38dd28731d8702eb4dcf515fc18059f
carvilsi <carvilsi.mail.list@gmail.com>
2024-05-09 11:51:11 +0200

    added moar faces

--- tests/main.test.js
+++ tests/main.test.js
@@ -3,7 +3,7 @@
 import test from 'ava';
 import facetxt from '../src/index.js';
 
-const TOTAL_FACES = 753;
+const TOTAL_FACES = 765;
 const WHOLE_EMBARRASSED_FACES = [
     ':$', '://)',
     '://3', '(^^ゞ',
